769de935be3f54f9e9da9ea6e77aee791faa8983
Benjamin Torio <beto3809@colorado.edu>
2026-03-24 00:37:08 -0500

    file move and rename - prep for njk convert

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("resources");
   eleventyConfig.addPassthroughCopy("resume.css");
   eleventyConfig.addPassthroughCopy("theme.js");
+  eleventyConfig.addPassthroughCopy("now.css");
 
   return {
     dir: {
